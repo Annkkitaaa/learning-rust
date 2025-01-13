ec561083bea599acceb6e0f35f7268db85eda79e
Annkkitaaa <ankitasingh15.102@gmail.com>
2025-01-13 18:51:54 +0530

    day2

---
+++ projects/variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\Ankita Singh\learning-rust\projects\variables\target\debug\deps\variables.exe: src/main.rs
+
+C:\Users\Ankita Singh\learning-rust\projects\variables\target\debug\deps\variables.d: src/main.rs
+
+src/main.rs:
---
+++ projects/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\Ankita\ Singh\learning-rust\projects\variables\target\debug\variables.exe: C:\Users\Ankita\ Singh\learning-rust\projects\variables\src\main.rs
