390ed5a06a0baded72b82e41fe936075b4e19a23
Annkkitaaa <ankitasingh15.102@gmail.com>
2025-01-14 14:54:00 +0530

    data types

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -3,4 +3,7 @@ fn main() {
     println!("the value of x is : {x}");
     x=6;
     println!("the value of x is : {x}");
+    let tup: (i32, f64, u8)= (500,6.4,1);
+    let (x,y,z) =tup;
+    println!("the vale of x is:{x} ");
 }
