5f0c38e56035d556a8508e09771959e2ceea902c
Annkkitaaa <ankitasingh15.102@gmail.com>
2025-01-14 22:34:40 +0530

    Update main.rs

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -7,11 +7,12 @@ fn main() {
     x=6;
     println!("the value of x is : {x}");
     let tup: (i32, f64, u8)= (500,6.4,1);
-    let (x,_y,_z)=tup;
+    let (x,_y,_z)=tup; //it was earlier showing error because y and z were completely not being used so putting an _ is telling the program that it was intentional.
     println!("the vale of x is:{x} ");
 
     let x = plus_one(5);
 
     println!("The value of x is: {x}");
+
      
 }
