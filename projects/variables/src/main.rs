378f439f27d3f2e94e0996435fcd10550bc94d02
Annkkitaaa <ankitasingh15.102@gmail.com>
2025-04-21 18:49:37 +0530

    update main.rs

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -1,49 +1,35 @@
-fn plus_one(x: i32) -> i32 {
-    x + 1
-}
-
-fn takes_ownership(some_string: String) { // some_string comes into scope
-    println!("{some_string}");
-} // Here, some_string goes out of scope and `drop` is called. The backing
-  // memory is freed.
-
-fn makes_copy(some_integer: i32) { // some_integer comes into scope
-    println!("{some_integer}");
-} // Here, some_integer goes out of scope. Nothing special happens.
-
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    let mut x=0;
-    println!("the value of x is : {x}");
-    x=6;
-    println!("the value of x is : {x}");
-    let tup: (i32, f64, u8)= (500,6.4,1);
-    let (x,_y,_z)=tup; //it was earlier showing error because y and z were completely not being used so putting an _ is telling the program that it was intentional.
+    println!("Guess the number!");
 
-    println!("the vale of x is:{x} ");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let x = plus_one(5);
+    loop {
+        println!("Please input your guess.");
 
-    println!("The value of x is: {x}");
+        let mut guess = String::new();
 
-    //variable scope
-    println!("we need to learn rust this week ")
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-       {                      // s is not valid here, it’s not yet declared
-        let s = "hello";   // s is valid from this point forward
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-        // do stuff with s
-    } 
+        println!("You guessed: {guess}");
 
-    let s = String::from("hello");  // s comes into scope
-
-    takes_ownership(s);             // s's value moves into the function...
-                                    // ... and so is no longer valid here
-
-    let x = 5;                      // x comes into scope
-
-    makes_copy(x);                  // x would move into the function,
-                    
-
-     
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
