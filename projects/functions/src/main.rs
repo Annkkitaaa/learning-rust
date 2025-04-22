0700afb5973b039888c86fab77e51f3f22f228bd
Annkkitaaa <ankitasingh15.102@gmail.com>
2025-04-22 12:34:16 +0530

    added return to functions

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -1,7 +1,16 @@
 fn main() {
+    let x = plus_one(5);
+
+    println!("The value of x is: {x}");
     another_function(5);
 }
 
 fn another_function(x: i32){
     println!("the value of x is {x}!");
+}
+
+
+
+fn plus_one(x: i32) -> i32 {
+    x + 1;
 }
